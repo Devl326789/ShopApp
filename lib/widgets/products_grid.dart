8af4b26cadbc19d62f1856d2fe3ec8ba731ea16d
Devl326789 <Devl987623@gmail.com>
2021-01-10 18:39:10 -0800

    First implementation of Provider

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+import '../providers/products.dart';
+import 'package:provider/provider.dart';
+import '../widgets/product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, i) => ProductItem(
+        products[i].id,
+        products[i].imageUrl,
+        products[i].title,
+      ),
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+    );
+  }
+}
