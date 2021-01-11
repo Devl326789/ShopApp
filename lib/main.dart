f720d468c4bcb27df62a6e50bcbf1f63c7af60e8
Devl326789 <Devl987623@gmail.com>
2021-01-11 09:45:26 -0800

    Displaying total in Cart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 import 'package:provider/provider.dart';
 import './providers/cart.dart';
+import './screens/cart_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -29,6 +30,7 @@ class MyApp extends StatelessWidget {
           home: ProductsOverviewScreen(),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            CartScreen.routeName: (ctx) => CartScreen(),
           }),
     );
   }
