abe1108f3d4cdbfd6e8cac485ffb2cde8f282788
Devl326789 <Devl987623@gmail.com>
2021-01-13 09:58:40 -0800

    Updating via Patch requests

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -121,9 +121,17 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url = 'https://shopapp-bb57b-default-rtdb.firebaseio.com/$id.json';
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price,
+          }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     }
